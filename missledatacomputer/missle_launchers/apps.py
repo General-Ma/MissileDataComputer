51d33a159d24c2b8c1d40e539709291a077d5cd8
General MA <kidmmz@hotmail.com>
2024-06-05 03:37:38 +1000

    init the proj and apps

---
+++ missledatacomputer/missle_launchers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MissleLaunchersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'missle_launchers'
---
+++ missledatacomputer/missles/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MisslesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'missles'
