cc1d4a1da535c7f2bd6da392e69ac1ec1bddc187
General MA <kidmmz@hotmail.com>
2024-06-06 05:02:30 +1000

    changed config to reflect renaming folders and projects

--- missiledatacomputer/missiledatacomputer/urls.py
+++ missiledatacomputer/missiledatacomputer/urls.py
@@ -1,4 +1,4 @@
-"""missledatacomputer URL Configuration
+"""missiledatacomputer URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
